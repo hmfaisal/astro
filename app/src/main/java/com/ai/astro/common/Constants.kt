3380530c1d7541c27f250514f83d5faa311bd686
Hasan Mahmud <hasan.mahmud@accenture.com>
2023-05-11 20:18:39 +0200

    integrate chat with astro app without data modeling

--- app/src/main/java/com/ai/astro/common/Constants.kt
+++ app/src/main/java/com/ai/astro/common/Constants.kt
@@ -3,6 +3,7 @@ package com.ai.astro.common
 object Constants {
     const val ASTRONAUT_BASE_URL = "https://ll.thespacedevs.com/2.2.0/"
     const val OPENAI_BASE_URL = "https://api.openai.com/"
+    const val OPENAI_TOKEN = "<YOUR_OPE_AI_API_KEY>"
     const val PAGE_SIZE = 10
     const val OPENAI_MODEL = "gpt-3.5-turbo"
 }
--- app/src/main/java/com/ai/astro/data/remote/openai/OpenAiApi.kt
+++ app/src/main/java/com/ai/astro/data/remote/openai/OpenAiApi.kt
@@ -1,5 +1,6 @@
 package com.ai.astro.data.remote
 
+import com.ai.astro.common.Constants.OPENAI_TOKEN
 import com.ai.astro.data.remote.openai.OpenAiRequestBody
 import com.ai.astro.data.remote.openai.OpenAiResponse
 import retrofit2.http.Body
@@ -7,7 +8,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface OpenAiApi {
-    @Headers("Content-Type: application/json", "Authorization: Bearer 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")
+    @Headers("Content-Type: application/json", "Authorization: Bearer $OPENAI_TOKEN")
     @POST("v1/chat/completions")
     suspend fun generateResponse(@Body requestBody: OpenAiRequestBody): OpenAiResponse
 }
